821bc3a035737ac80e0bee62a7cfaf7b36667873
callarose4 <crkolar@syr.edu>
2026-01-28 20:08:39 +0000

    attempt to fix defining stream

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -47,11 +47,11 @@ if uploaded_file and question:
             stream=True,
         )
 
-        def stream_text ():
+        def stream_text (stream):
             for event in stream:
                   delta = event.choices[0].delta.content
                   if delta: 
                         yield delta
 
         # Stream the response to the app using `st.write_stream`.
-st.write_stream(stream)
+st.write_stream(stream_text(stream))
