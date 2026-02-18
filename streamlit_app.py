b07592017b229c74f72a72dbb1ee1d298bc96d36
callarose4 <crkolar@syr.edu>
2026-02-18 14:38:26 +0000

    added lab 5 and weather api key

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 lab1_page = st.Page("Labs/Lab1.py", title="Lab 1")
 lab2_page = st.Page("Labs/Lab2.py", title="Lab 2")
 lab3_page = st.Page("Labs/Lab3.py", title="Lab 3")
-lab4_page = st.Page("Labs/Lab4.py", title="Lab 4", default=True) #default to Lab 4
+lab4_page = st.Page("Labs/Lab4.py", title="Lab 4")
+lab5_page = st.Page("Labs/Lab5.py", title="Lab 5", default=True)
 
 
 pg = st.navigation([lab1_page, lab2_page, lab3_page, lab4_page])
