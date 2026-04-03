fcf6ab60f976690e9d2c46a47c458d3820ee9b79
callarose4 <crkolar@syr.edu>
2026-04-03 22:13:09 +0000

    changed default

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ lab2_page = st.Page("Labs/Lab2.py", title="Lab 2")
 lab3_page = st.Page("Labs/Lab3.py", title="Lab 3")
 lab4_page = st.Page("Labs/Lab4.py", title="Lab 4")
 lab5_page = st.Page("Labs/Lab5.py", title="Lab 5")
-lab6_page = st.Page("Labs/Lab6.py", title="Lab 6", default=True)
+lab6_page = st.Page("Labs/Lab6.py", title="Lab 6")
 lab7_page = st.Page("Labs/Lab7.py", title="Lab 7", default=True)
 
 pg = st.navigation([lab1_page, lab2_page, lab3_page, lab4_page, lab5_page, lab6_page, lab7_page])
